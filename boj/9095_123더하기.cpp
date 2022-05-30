17573c4402c8cef7ec28cd0f9b447d8bcadf0da3
김남훈 <85011551+namhoon21@users.noreply.github.com>
2022-05-30 18:36:53 +0900

    Create 9095_123더하기.cpp

---
+++ boj/9095_123더하기.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+int main()
+{
+	freopen("test.txt", "r", stdin);
+	int T;
+	cin >> T;
+	int arr[12];
+	arr[1] = 1;
+	arr[2] = 2;
+	arr[3] = 4;
+	for (int i = 4; i < 12; i++)
+	{
+		arr[i] = arr[i - 3] + arr[i - 2] + arr[i - 1];
+	}
+	for (int i = 0; i < T; i++)
+	{
+		int n;
+		cin >> n;
+		cout << arr[n] << '\n';
+	}
+
+
+	return 0;
+}
